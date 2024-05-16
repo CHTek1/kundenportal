eef476966222ffbcbdc5cdb89b06308aa234a3e3
CHTek1 <63433073+CHTek1@users.noreply.github.com>
2024-05-17 01:14:14 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
+// Debug
 alert("Hello FROM chtek git");
+
+/*
+
+let copyrightFooter = document.querySelector('footer.bg-white > section:nth-child(1)');
+copyrightFooter.innerHTML = '<span href="https://invoice.chtek.de" target="_blank" class="text-xs md:text-sm text-gray-700">Copyright © 2024 <a class="text-primary hover:underline" href="https://invoice.chtek.de" target="_blank">CHTek</a>. </span>'
+*/
